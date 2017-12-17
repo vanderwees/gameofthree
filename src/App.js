d9d164832817284570ccd38227e8bd529163d0ca
Bernard <bernard@vdwe.es>
2017-12-17 16:42:08 +0100

    Added material-ui theme provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,18 +10,20 @@ import Server from './Server/Server.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">A Game of Three</h1>
-        </header>
-        <p className="App-intro">
-          Intro text goes here.
-        </p>
-        <p className="App-body">
-          <Client className="App-child" />
-          <Server className="App-child" />
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">A Game of Three</h1>
+          </header>
+          <p className="App-intro">
+            Intro text goes here.
+          </p>
+          <p className="App-body">
+            <Client className="App-child" />
+            <Server className="App-child" />
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
